ec8eeb5fc908d829c779c178aaf0024d9a2dc79a
luca <luca.sommer@stud.hslu.ch>
2020-11-21 23:52:35 +0100

    SearchBar, Releases DetailView

--- TvTracker/Views/ReleaseView.swift
+++ TvTracker/Views/ReleaseView.swift
@@ -11,7 +11,6 @@ import URLImage
 struct ReleaseView: View {
     
     @State var shows = [ShowModel]()
-    
     @State var searchText = ""
     
     let showService = ShowService()
@@ -23,26 +22,33 @@ struct ReleaseView: View {
     }
     
     var body: some View {
-        VStack {
-            SearchBar(text: $searchText).padding(.top, 10)
-            
-            List(shows) {
-                show in
-                HStack {
-                    ListImageView(url: show.imageURL).frame(width: 68, height: 100).cornerRadius(8).shadow(radius: 4)
-                    VStack {
-                        HStack {
-                            Text(show.title).frame(maxWidth: .infinity, alignment: .leading).font(.system(size: 18))
-                            Image(systemName: "star.fill").foregroundColor(Color(red: 1, green: 0.85, blue: 0)).padding(.trailing, 10)
-                            
-                        }.padding(.bottom, 10)
-                        Text(show.overview).frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 80, alignment: .topLeading).truncationMode(.tail).font(.system(size: 12))
-                        
-                    }.padding(.leading, 10)
-                }
-            }.onAppear(perform: getSeries)
-        }
         
+        NavigationView {
+            List() {
+                SearchBar(text: $searchText)
+                
+                ForEach(shows) {
+                show in
+                NavigationLink(
+                    destination: /* HIER RICHTIGE VIEW*/ EmptyView()) {
+                        HStack {
+                            ListImageView(url: show.imageURL).frame(width: 68, height: 100).cornerRadius(8)
+                            VStack {
+                                HStack {
+                                    Text(show.title).frame(maxWidth: .infinity, alignment: .leading).font(.system(size: 18))
+                                    Image(systemName: "star.fill").foregroundColor(Color(red: 1, green: 0.85, blue: 0)).padding(.trailing, 10)
+                                    
+                                }.padding(.bottom, 10)
+                                Text(show.overview).frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 80, alignment: .topLeading).truncationMode(.stail).font(.system(size: 12))
+                                
+                            }.padding(.leading, 10)
+                        }
+                }
+                    
+                }
+            }.onAppear(perform: getSeries) //CHECKEN WEIL IMMER NEU HOLT BEI APPEAR 10*x
+            .navigationBarTitle("Releases")
+        }
        
     }
 }
