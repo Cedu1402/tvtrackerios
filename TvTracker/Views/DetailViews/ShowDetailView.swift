835e4b12269b1697ea321db47ce997eb6332f527
luca <luca.sommer@stud.hslu.ch>
2020-12-06 16:27:27 +0100

    bugfix

--- TvTracker/Views/DetailViews/ShowDetailView.swift
+++ TvTracker/Views/DetailViews/ShowDetailView.swift
@@ -37,10 +37,6 @@ struct ShowDetailView: View {
                 Text(show.title).font(.title)
                     .bold()
                 
-                FavoriteStarView(show: show){
-                    dataSource.changeFavoriteFlag(show: show)
-                }.environmentObject(dataSource)
-                
                 Text(show.overview)
                     .padding(.top, 10)
                     .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
